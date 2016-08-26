dd563297d38bc34813e4c2f58fafda5e6dd3ed70
xferra <xferra@live.com>
2016-08-26 06:11:09 -0700

    Fix and simplify Json based Linters

---
+++ src/Metrics.Integrations.Linters/Extensions/Deserialize.cs
@@ -0,0 +1,18 @@
+﻿namespace Metrics.Integrations.Extensions
+{
+    using System.IO;
+    using Newtonsoft.Json;
+
+    public static class Deserialize
+    {
+        public static T DeserializeAsJson<T>(this Stream self)
+        {
+            var serializer = new JsonSerializer();
+            using (var reader = new StreamReader(self))
+            using (var jsonTextReader = new JsonTextReader(reader))
+            {
+                return serializer.Deserialize<T>(jsonTextReader);
+            }
+        }
+    }
+}
--- src/Metrics.Integrations.Linters/Linters/phpcs/Lint.cs
+++ src/Metrics.Integrations.Linters/Linters/phpcs/Lint.cs
@@ -1,18 +1,17 @@
-using System;
-using System.IO;
-using Metrics.Integrations.Linters;
-using Newtonsoft.Json;
-
 namespace Metrics.Integrations.Linters.Phpcs
 {
+    using System.IO;
+    using Extensions;
+
     public class Lint : Linter
     {
-        public override ILinterResult Parse(Stream stream){
-            string json = new StreamReader(stream).ReadToEnd();
-            return JsonConvert.DeserializeObject<LintResult>(json);
+        public override ILinterResult Parse(Stream stream)
+        {
+            return stream.DeserializeAsJson<LintResult>();
         }
 
-        public override ILinterModel Map(ILinterResult result) {
+        public override ILinterModel Map(ILinterResult result)
+        {
             return (LintResult)result;
         }
     }
