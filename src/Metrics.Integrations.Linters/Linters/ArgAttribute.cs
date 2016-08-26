22cd39a724289ff5510651ea9da3a239d9cf16e7
xferra <xferra@live.com>
2016-08-26 05:59:38 -0700

    Simplify ArgAttribute params

--- src/Metrics.Integrations.Linters/Linters/ArgAttribute.cs
+++ src/Metrics.Integrations.Linters/Linters/ArgAttribute.cs
@@ -6,17 +6,17 @@
     {
         public string Name { get; }
 
-        public string ValueSeparator { get; }
+        public string Separator { get; }
 
-        public bool AddValue { get; }
+        public bool Add { get; }
 
         public int Order { get; }
 
-        public ArgAttribute(string name = null, bool addValue = true, string valueSeparator = " ", int order = int.MinValue)
+        public ArgAttribute(string name = null, bool add = true, string separator = " ", int order = int.MinValue)
         {
             Name = name;
-            AddValue = addValue;
-            ValueSeparator = valueSeparator;
+            Add = add;
+            Separator = separator;
             Order = order;
         }
     }
--- src/Metrics.Integrations.Linters/Linters/phpcs/LintArgs.cs
+++ src/Metrics.Integrations.Linters/Linters/phpcs/LintArgs.cs
@@ -1,6 +1,3 @@
-using System;
-using Metrics.Integrations.Linters;
-
 namespace Metrics.Integrations.Linters.Phpcs
 {
     public class LintArgs : ILinterArgs
@@ -8,67 +5,67 @@ namespace Metrics.Integrations.Linters.Phpcs
         /// <summary>
         /// Report type. Default: JSON
         /// </summary>
-        [Arg("--report", valueSeparator: "=", order: 1)]
+        [Arg("--report", separator: "=", order: 1)]
         public string ReportType { get; set; } 
 
         /// <summary>
         /// Extensions to check. By default .inc, .php, .js and .css
         /// </summary>
-        [Arg("--extensions", valueSeparator: "=", order: 1)]
+        [Arg("--extensions", separator: "=", order: 1)]
         public string Extensions { get; set; } 
 
         /// <summary>
         /// Files and directories to ignore
         /// </summary>
-        [Arg("--ignore", valueSeparator: "=", order: 1)]
+        [Arg("--ignore", separator: "=", order: 1)]
         public string Ignore { get; set; } 
 
         /// <summary>
         /// Standard to check (For example PEAR)
         /// </summary>
-        [Arg("--standard", valueSeparator: "=", order: 1)]
+        [Arg("--standard", separator: "=", order: 1)]
         public string Standard { get; set; }
 
         /// <summary>
         /// List of sniffs to use. By default use all
         /// </summary>
-        [Arg("--sniffs", valueSeparator: "=", order: 1)]
+        [Arg("--sniffs", separator: "=", order: 1)]
         public string Sniffs { get; set; } 
 
         /// <summary>
         /// List of sniffs to ignore
         /// </summary>
-        [Arg("--exclude", valueSeparator: "=", order: 1)]
+        [Arg("--exclude", separator: "=", order: 1)]
         public string ExcludeSniffs { get; set; } 
 
         /// <summary>
         /// Severity. For example if you set 3, phpcs will hide all errors and warnings with a severity less than 3
         /// </summary>
-        [Arg("--severity", valueSeparator: "=", order: 1)]
+        [Arg("--severity", separator: "=", order: 1)]
         public string Severity { get; set; } 
 
         /// <summary>
         /// Error Severity
         /// </summary>
-        [Arg("--error-severity", valueSeparator: "=", order: 1)]
+        [Arg("--error-severity", separator: "=", order: 1)]
         public string ErrorSeverity { get; set; } 
 
         /// <summary>
         /// Warning Severity
         /// </summary>
-        [Arg("--warning-severity", valueSeparator: "=", order: 1)]
+        [Arg("--warning-severity", separator: "=", order: 1)]
         public string WarningSeverity { get; set; }
 
         /// <summary>
         /// Set encoding (by default ISO-8859-1)
         /// </summary>
-        [Arg("--encoding", valueSeparator: "=", order: 1)]
+        [Arg("--encoding", separator: "=", order: 1)]
         public string Encoding { get; set; }
 
         /// <summary>
         /// Include one or more custom bootstrap files before beginning the run
         /// </summary>
-        [Arg("--bootstrap", valueSeparator: "=", order: 1)]
+        [Arg("--bootstrap", separator: "=", order: 1)]
         public string Bootstrap { get; set; } 
  
 
