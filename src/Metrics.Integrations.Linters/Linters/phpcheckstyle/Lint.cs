063664463515f49a7d180a74af24730319f03a8e
Oleg Safonov <by.xsofa@gmail.com>
2016-09-16 18:45:56 +0300

    Use lfm in phpcheckstyle

--- src/Metrics.Integrations.Linters/Linters/phpcheckstyle/Lint.cs
+++ src/Metrics.Integrations.Linters/Linters/phpcheckstyle/Lint.cs
@@ -12,7 +12,41 @@ namespace Metrics.Integrations.Linters.phpcheckstyle
 
         public override ILinterModel Map(ILinterResult result)
         {
-            return (ILinterModel)result;
+            var res = (LintResult)result;
+            LinterFileModel lfm = new LinterFileModel();
+            foreach(File f in res.FilesList)
+            {
+                LinterFileModel.File lf = new LinterFileModel.File
+                {
+                    Path = f.FilePath
+                };
+                foreach (Error e in f.ErrorsList)
+                    lf.Errors.Add(new LinterError {
+                        Severity = e.Severity,
+                        Message = e.Message,
+                        Rule = new LinterFileModel.Rule
+                        {
+                            Name = e.Source
+                        },
+                        Row = new LinterFileModel.Interval
+                        {
+                            Start = System.Int32.Parse(e.Line),
+                            End = System.Int32.Parse(e.Line)
+                        },
+                        Column = new LinterFileModel.Interval
+                        {
+                            Start = System.Int32.Parse(e.Column),
+                            End = System.Int32.Parse(e.Column)
+                        }
+                    });
+                // TODO: remove similar elements
+            }
+            return lfm;
+        }
+
+        public class LinterError : LinterFileModel.Error
+        {
+            public string Severity;
         }
 
     }
