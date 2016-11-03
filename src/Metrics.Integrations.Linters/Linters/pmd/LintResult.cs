2a9f9c2d8b0b54ecf5a1ee9b3366407146ed2d79
Roman <romanrostislavovich@gmail.com>
2016-11-03 16:31:18 +0300

    Add pmd linter

---
+++ src/Metrics.Integrations.Linters/Linters/pmd/LintResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Metrics.Integrations.Linters.pmd
+{
+    using System.Xml.Serialization;
+    using System.Collections.Generic;
+
+    [XmlRoot(ElementName = "pmd")]
+    public class LintResult : ILinterResult
+    {
+        [XmlElement("file")]
+        public List<File> Files { get; set; } 
+    }
+}
