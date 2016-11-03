78c9a31054fadd20901407bb62624e3ea05a95e1
Oleg Safonov <by.xsofa@gmail.com>
2016-11-03 14:06:19 +0300

    Fix with IDisposable

--- src/Metrics.Integrations.Linters/Linters/pep8/Lint.cs
+++ src/Metrics.Integrations.Linters/Linters/pep8/Lint.cs
@@ -8,18 +8,21 @@ namespace Metrics.Integrations.Linters.pep8
     {
         public override ILinterResult Parse(Stream stream)
         {
-            var output = (new StreamReader(stream)).ReadToEnd();
-            MatchCollection matches = Regex.Matches(output, @".\/(.*)\:([0-9]*): \[(.*)\] (.*)");
-            return new LintResult
+            using (StreamReader sr = new StreamReader(stream))
             {
-                WarningsList = matches.Cast<Match>().Select(match => new Warning
+                var output = sr.ReadToEnd();
+                MatchCollection matches = Regex.Matches(output, @".\/(.*)\:([0-9]*): \[(.*)\] (.*)");
+                return new LintResult
                 {
-                    Description = match.Groups[4].Value + ".",
-                    FilePath = match.Groups[1].Value,
-                    Line = match.Groups[2].Value,
-                    Pattern = match.Groups[3].Value
-                }).ToList()
-            };
+                    WarningsList = matches.Cast<Match>().Select(match => new Warning
+                    {
+                        Description = match.Groups[4].Value + ".",
+                        FilePath = match.Groups[1].Value,
+                        Line = match.Groups[2].Value,
+                        Pattern = match.Groups[3].Value
+                    }).ToList()
+                };
+            }
         }
 
         public override ILinterModel Map(ILinterResult result)
