79c41e794f69e9b4f3059da3a41fb03ce3432a75
xferra <xferra@live.com>
2016-11-11 16:01:06 +0300

    Publish script for docker image

--- publish.sh
+++ publish.sh
@@ -3,3 +3,7 @@ dotnet publish src/cli/project.json --output bin/osx.10.11-x64/ -c release -r os
 dotnet publish src/cli/project.json --output bin/debian.8-x64/ -c release -r debian.8-x64
 dotnet publish src/cli/project.json --output bin/win8-x64/ -c release -r win8-x64
 dotnet publish src/cli/project.json --output bin/win10-x64/ -c release -r win10-x64
+
+docker build -t linterhub-cli .
+mkdir bin/docker
+docker save linterhub-cli > bin/docker/image.tar
