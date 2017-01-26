d0c4ed423d40a78dcef18c5a8cc5380006906f9c
Oleg Safonov <by.xsofa@gmail.com>
2017-01-26 17:40:29 +0300

    Now CatalogStartegy returns ative linters

--- src/cli/Strategy/CatalogStrategy.cs
+++ src/cli/Strategy/CatalogStrategy.cs
@@ -5,21 +5,32 @@ namespace Linterhub.Cli.Strategy
     using Runtime;
     using Engine;
     using Engine.Exceptions;
+    using System.IO;
     using Engine.Extensions;
+    using Newtonsoft.Json;
 
     public class CatalogStrategy : IStrategy
     {
         public object Run(RunContext context, LinterFactory factory, LogManager log)
         {
             var catalog = GetCatalog(context, factory);
+            ProjectConfig config = null;
+            if(context.Project != null)
+            {
+                var projectConfigFile = context.GetProjectConfigPath();
+                var existFileConfig = File.Exists(projectConfigFile);
+                config = context.GetProjectConfig();
+            }
             var result =
                 from record in factory.GetRecords().OrderBy(x => x.Name)
                 let item = catalog.linters.FirstOrDefault(y => y.name == record.Name)
+                let conf_item = config.Linters.FirstOrDefault(y => y.Name == record.Name)
                 select new
                 {
                     name = record.Name,
                     description = item?.description,
-                    languages = item?.languages
+                    languages = item?.languages,
+                    active = context.Project != null ? (conf_item?.Active != null ? conf_item?.Active : false) : null
                 };
 
             return result;
