0458ecf6f3fe4874c8537da0195a2fcbf84bef7e
xferra <xferra@live.com>
2017-02-03 00:23:35 +0300

    Improve setup and ci scripts

---
+++ script/cibuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Nuget is not needed right now
+# dotnet pack src/engine/project.json -c Release -o bin
+Version='0.3.3'
+
+sh script/package.native.sh dotnet $Version
+sh script/package.sh osx.10.11-x64 $Version
+sh script/package.sh win8-x64 $Version
+sh script/package.sh win10-x64 $Version
+sh script/package.sh debian.8-x64 $Version
+sh script/package.sh ubuntu.14.04-x64 $Version
+sh script/package.sh ubuntu.16.04-x64 $Version
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-TestPath=C:\Users\Andrey\Desktop\linterhub-tests
-
-Current=$PWD
-echo $Current
-cd bin/dotnet
-./cli --mode=analyze --linter=jshint --project=$TestPath\jquery
-
-cd $Current
