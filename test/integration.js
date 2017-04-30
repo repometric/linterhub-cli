c8063c8ca18548f5cb6444febfe9f5ad292d9be1
Oleg Safonov <by.xsofa@gmail.com>
2017-04-30 17:53:50 +0300

    Add tests for stylelint, fix config

--- test/integration.js
+++ test/integration.js
@@ -4,14 +4,14 @@ const _process = require('child_process');
 
 var integration = path.join(__dirname, 'integration');
 
-function printError(s)
+function printError(f, s)
 {
-    console.error("\x1b[31m%s\x1b[0m", s);
+    console.error("%s: \x1b[31m%s\x1b[0m", f, s);
 }
 
-function printSuccess(s)
+function printSuccess(f, s)
 {
-    console.error("\x1b[32m%s\x1b[0m", s);
+    console.error("%s: \x1b[32m%s\x1b[0m", f, s);
 }
 
 fs.readdir(integration, (err, files) => {
@@ -22,23 +22,30 @@ fs.readdir(integration, (err, files) => {
             _process.exec('sh ' + filePath, {
                     cwd: path.join(__dirname, '../bin/dotnet')
                 }, function(error, stdout, stderr) {
-                    var expected = JSON.parse(fs.readFileSync(path.join(integration, 'expected', file + ".log"), 'utf8'));
+                    var expected = "";
+                    try {
+                        expected = JSON.parse(fs.readFileSync(path.join(integration, 'expected', file + ".log"), 'utf8'));
+                    }
+                    catch(e){
+                        printError(file, "can't parse expected output");
+                        process.exit(1);
+                    }
                     fs.writeFileSync(path.join(integration, 'actual', file + ".log"), stdout);
                     var result = "";
                     try {
                         result = JSON.parse(stdout);
                     }
                     catch(e){
-                        printError(file + ": can't parse output");
+                        printError(file, "can't parse command output");
                         process.exit(1);
                     }
                     if(JSON.stringify(result) == JSON.stringify(expected))
                     {
-                        printSuccess(file + ": PASSED!");
+                        printSuccess(file, "PASSED!");
                     }
                     else
                     {
-                        printError(file + ": FAILED!");
+                        printError(file, "FAILED!");
                         _process.execSync('json-diff ' + path.join(integration, 'actual', file + ".log") + ' ' + path.join(integration, 'expected', file + ".log"), {stdio:[0,1,2]});
                         process.exit(1);
                     }
