fd0954e912e70eb3edfd73e0dca16e0e7003dc61
Oleg Safonov <by.xsofa@gmail.com>
2017-04-30 19:39:10 +0300

    Add tslint and standard tests

--- src/cli/linterhub/tslint/reporter.js
+++ src/cli/linterhub/tslint/reporter.js
@@ -14,33 +14,46 @@ process.stdin.on('data', function (chunk) {
     lines.forEach(processLine);
 });
 
+process.stdin.on('end', function () {
+    console.log(JSON.stringify(result));
+});
+
 var result = [];
 
 function processLine(line) {
-    var data = JSON.parse(line);
-    data.forEach(function (problem) {
-        var obj = result.find(function (element, index, array) {
-            return problem.name === element.filePath;
-        });
-        if (obj === undefined) {
-            obj = {
-                filePath: problem.name,
-                messages: []
-            };
-            result.push(obj);
-        }
-        obj.messages.push({
-            message: problem.failure,
-            severity: problem.ruleSeverity === "ERROR" ? "error" : "warning",
-            line: problem.startPosition.line,
-            lineEnd: problem.endPosition.line,
-            column: problem.startPosition.character,
-            columnEnd: problem.endPosition.character,
-            ruleId: "tslint:" + problem.ruleName,
-            ruleName: problem.ruleName
-        });
+    var regex = /(.*): \((.*)\) (.*)\[([0-9]+), ([0-9]+)\]\: (.*)/g;
+    var match = regex.exec(line);
+
+    if (match == null) {
+        return;
+    }
+
+    var problem = {
+        message: match[6],
+        severity: match[1] === "ERROR" ? "error" : "warning",
+        line: match[4] - 1,
+        lineEnd: match[4] - 1,
+        column: match[5] - 1,
+        columnEnd: 1000,
+        ruleId: "standard:" + match[2],
+        ruleName: match[2]
+    }
+
+    var filePath = match[3].trim();
+
+    var obj = result.find(function (element, index, array) {
+        return filePath === element.filePath;
     });
-    print_result();
+
+    if (obj === undefined) {
+        obj = {
+            filePath: filePath,
+            messages: []
+        };
+        result.push(obj);
+    }
+
+    obj.messages.push(problem);
 }
 
 function print_result() {
