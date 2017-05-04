76cb4253a070e6d0a32c2a0db3ec1b722f801be7
Oleg Safonov <by.xsofa@gmail.com>
2017-05-04 10:32:30 +0000

    Check linters versions

--- test/integration.js
+++ test/integration.js
@@ -6,12 +6,17 @@ var integration = path.join(__dirname, 'integration');
 
 function printError(f, s)
 {
-    console.error("%s: \x1b[31m%s\x1b[0m", f, s);
+    console.error("\x1b[37m%s: \x1b[31m%s\x1b[37m", f, s);
 }
 
 function printSuccess(f, s)
 {
-    console.error("%s: \x1b[32m%s\x1b[0m", f, s);
+    console.error("\x1b[37m%s: \x1b[32m%s\x1b[37m", f, s);
+}
+
+function printWarning(f, s)
+{
+    console.error("\x1b[37m%s: \x1b[33m%s\x1b[37m", f, s);
 }
 
 fs.readdir(integration, (err, files) => {
@@ -22,33 +27,51 @@ fs.readdir(integration, (err, files) => {
             _process.exec('sh ' + filePath, {
                     cwd: path.join(__dirname, '../bin/dotnet')
                 }, function(error, stdout, stderr) {
-                    var expected = "";
+                    var expected = fs.readFileSync(path.join(integration, 'expected', file + ".log"), 'utf8');
+                    var is_json = true;
                     try {
-                        expected = JSON.parse(fs.readFileSync(path.join(integration, 'expected', file + ".log"), 'utf8'));
+                        expected = JSON.parse(expected);
                     }
                     catch(e){
-                        printError(file, "can't parse expected output");
-                        process.exit(1);
+                        printWarning(file, "can't parse expected output as json");
+                        is_json = false;
+                        //process.exit(1);
                     }
                     fs.writeFileSync(path.join(integration, 'actual', file + ".log"), stdout);
-                    var result = "";
-                    try {
-                        result = JSON.parse(stdout);
-                    }
-                    catch(e){
-                        printError(file, "can't parse command output:");
-                        console.log(stdout);
-                        process.exit(1);
-                    }
-                    if(JSON.stringify(result) == JSON.stringify(expected))
+                    var result = stdout;
+                    if(is_json)
                     {
-                        printSuccess(file, "PASSED!");
+                        try {
+                            result = JSON.parse(stdout);
+                            if(JSON.stringify(result) == JSON.stringify(expected))
+                            {
+                                printSuccess(file, "PASSED!");
+                            }
+                            else
+                            {
+                                printError(file, "FAILED!");
+                                _process.execSync('json-diff ' + path.join(integration, 'actual', file + ".log") + ' ' + path.join(integration, 'expected', file + ".log"), {stdio:[0,1,2]});
+                                process.exit(1);
+                            }
+                        }
+                        catch(e){
+                            printError(file, "can't parse command output:");
+                            console.log(stdout);
+                            process.exit(1);
+                        }
                     }
                     else
                     {
-                        printError(file, "FAILED!");
-                        _process.execSync('json-diff ' + path.join(integration, 'actual', file + ".log") + ' ' + path.join(integration, 'expected', file + ".log"), {stdio:[0,1,2]});
-                        process.exit(1);
+                        if(result == expected)
+                        {
+                            printSuccess(file, "PASSED!");
+                        }
+                        else
+                        {
+                            printError(file, "FAILED!");
+                            _process.execSync('sh diff -a' + path.join(integration, 'actual', file + ".log") + ' ' + path.join(integration, 'expected', file + ".log"), {stdio:[0,1,2]});
+                            process.exit(1);
+                        }
                     }
                 }
             );
---
+++ test/integration/linters.sh
@@ -0,0 +1,10 @@
+eslint --version
+coffeelint --version
+csslint --version
+jshint --version
+standard --version
+htmlhint --version
+jslint --version
+stylelint --version
+tslint --version 
+sass-lint --version
