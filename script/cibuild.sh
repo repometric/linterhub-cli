6325c3f9bdd7676f36be5a32dde91678f553ae20
Oleg Safonov <by.xsofa@gmail.com>
2017-05-04 10:34:21 +0000

    Build only dotnet

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -5,9 +5,9 @@
 Version='0.4.0'
 
 sh script/package.native.sh dotnet $Version
-sh script/package.sh osx.10.11-x64 $Version
-sh script/package.sh win8-x64 $Version
-sh script/package.sh win10-x64 $Version
-sh script/package.sh debian.8-x64 $Version
-sh script/package.sh ubuntu.14.04-x64 $Version
-sh script/package.sh ubuntu.16.04-x64 $Version
+#sh script/package.sh osx.10.11-x64 $Version
+#sh script/package.sh win8-x64 $Version
+#sh script/package.sh win10-x64 $Version
+#sh script/package.sh debian.8-x64 $Version
+#sh script/package.sh ubuntu.14.04-x64 $Version
+#sh script/package.sh ubuntu.16.04-x64 $Version
