a1a357c44000921a0a3f1511d0efa01a74ce018c
xferra <xferra@live.com>
2017-05-07 13:41:39 +0300

    Fix build and add type extension package

--- script/format.sh
+++ script/format.sh
@@ -1,3 +1,4 @@
 node script/js/format.js src/hub/.schema/linterhub.output.json
+node script/js/format.js src/hub/.schema/linterhub.config.json
 node script/js/format.js src/hub/.schema/engine.output.json
 node script/js/format.js src/hub/.schema/engine.json
