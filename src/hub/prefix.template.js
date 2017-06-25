898f0e88707158971e3727904640bec1e2df08fe
Oleg Safonov <by.xsofa@gmail.com>
2017-06-26 00:02:14 +0300

    Use eslint rules, update linters, fix schema path

--- src/hub/prefix.template.js
+++ src/hub/prefix.template.js
@@ -2,29 +2,32 @@ let fs = require('fs');
 let path = require('path');
 
 module.exports = {
-    run: function (extname, executeForFile, afterAll) {
-
+    run: function(extname, executeForFile, afterAll) {
         let result = [];
-
-        if (process.argv.findIndex((x) => x === "*") != -1)
+        if (process.argv.findIndex((x) => x === '*') != -1) {
             this.getFiles(process.cwd(), null, extname).forEach((x) => executeForFile(x, result));
-        else
-            executeForFile("", result);
-        if(afterAll)
+        } else {
+            executeForFile('', result);
+        }
+        if (afterAll) {
             afterAll(result);
+        }
     },
-    getFiles: function (dir, files_, extname) {
+    getFiles: function(dir, files_, extname) {
         files_ = files_ || [];
         let files = fs.readdirSync(dir);
         for (let i in files) {
-            let name = dir + '/' + files[i];
-            if (fs.statSync(name).isDirectory()) {
-                this.getFiles(name, files_, extname);
-            } else {
-                if (path.extname(name) === extname)
-                    files_.push(name);
+            if (typeof i == 'string') {
+                let name = dir + '/' + files[i];
+                if (fs.statSync(name).isDirectory()) {
+                    this.getFiles(name, files_, extname);
+                } else {
+                    if (path.extname(name) === extname) {
+                        files_.push(name);
+                    }
+                }
             }
         }
         return files_;
-    }
-}
+    },
+};
