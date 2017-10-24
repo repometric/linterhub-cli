5e181e2e96a23657f35b20d094ebe5fb7f0c9854
Oleg Safonov <by.xsofa@gmail.com>
2017-10-24 17:52:37 +0300

    Update scripts

--- script/package.native.sh
+++ script/package.native.sh
@@ -1,4 +1,4 @@
-dotnet restore src/cli/project.json && \
-dotnet publish src/cli/project.json --output bin/$1/ -f netcoreapp1.1 -c release && \
-zip -r bin/linterhub-cli-$1.zip bin/$1 #&& \
+dotnet restore src/cli/cli.csproj && \
+dotnet publish src/cli/cli.csproj --output ../../bin/$1/ -f netcoreapp2.0 -c release
+#zip -r bin/linterhub-cli-$1.zip bin/$1 #&& \
 #rm -R bin/$1
--- script/package.sh
+++ script/package.sh
@@ -1,4 +1,4 @@
-dotnet restore src/cli/project.json && \
-dotnet publish src/cli/project.json --output bin/$1/ -c release -r $1 -f netstandard1.6.1 && \
-zip -r bin/linterhub-cli-$1.zip bin/$1 && \
-rm -R bin/$1
+dotnet restore src/cli/cli.csproj && \
+dotnet publish src/cli/cli.csproj --output ../../bin/$1/ -c release -r $1 -f netstandard2.0
+#zip -r bin/linterhub-cli-$1.zip bin/$1 && \
+#rm -R bin/$1
